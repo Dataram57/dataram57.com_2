93962fdf09a832501ed2a0c10120aaf433f4e15f
Dataram57 <dataram66657@gmail.com>
2025-07-24 17:54:22 +0200

    bake.md

--- bake_md.py
+++ bake_md.py
@@ -1,8 +1,47 @@
+import os
+from pathlib import Path
+import markdown
+
+################################################################
+
+used = False
+
+def GetArticleFileName(i):
+    return ("article." + str(i) + ".md")
+
+################################################################
 
 def Start(path):
-    print("start")
-    return
+    global used
+    
+    #skip if file already exist
+    if (path / "main.html").is_file():
+        return
+        
+    #count files
+    i = 0
+    while (path / GetArticleFileName(i)).is_file():
+        i += 1
+
+    #count down and write markdown into main.html
+    html = ""
+    with open(path / "main.html", "w", encoding="utf-8") as f:
+        used = True
+        while i > 0:
+            i -= 1
+            print("Including: " + str(path / GetArticleFileName(i)))
+            with open(path / GetArticleFileName(i), "r", encoding="utf-8") as f2:
+                f.write("<article>")
+                f.write(markdown.markdown(f2.read()))
+                f.write("</article>")
+        
+
+################################################################
 
 def End(path):
-    print("end")
-    return 
+    global used
+    #check if was used
+    if used:
+        used = False
+        #delete generated file
+        os.remove(path / "main.html")
